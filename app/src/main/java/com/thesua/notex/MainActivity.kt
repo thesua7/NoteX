6e95f4a4570bf976f8ce4a79dd087b72a268678d
thesua7 <sams.ahamed@northsouth.edu>
2023-12-01 09:23:30 +0600

    theme issue fixed

--- app/src/main/java/com/thesua/notex/MainActivity.kt
+++ app/src/main/java/com/thesua/notex/MainActivity.kt
@@ -2,6 +2,7 @@ package com.thesua.notex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.thesua.notex.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         setContentView(binding.root)
     }
